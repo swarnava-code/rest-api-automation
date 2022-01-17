1c7466e62a6ca260773037ac27be138494e3b118
swarnava.18 <swarnava.18@mountblue.tech>
2022-01-17 18:50:40 +0530

    dependencies added, sample test case execute

---
+++ src/test/java/com/restassured/swarnava/MainTest.java
@@ -0,0 +1,14 @@
+package com.restassured.swarnava;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class MainTest {
+
+    @Test
+    void Test(){
+        assertEquals(5, 5);
+    }
+
+}
