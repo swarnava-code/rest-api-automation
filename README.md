00f49682500751c91c0e42a651e92e729f50e620
swarnava.18 <swarnava.18@mountblue.tech>
2022-01-17 22:12:49 +0530

    Jokes API methods implemented and passed

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Tested API #
 
 - The Users API is available at `https://reqres.in`
+- The Jokes API is available at `https://v2.jokeapi.dev`
--- src/test/java/com/restassured/swarnava/TestJokesAPI.java
+++ src/test/java/com/restassured/swarnava/TestJokesAPI.java
@@ -1,4 +1,67 @@
 package com.restassured.swarnava;
 
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
 public class TestJokesAPI {
+    final String baseUrl = "https://v2.jokeapi.dev";
+
+    @Test
+    void testGetMiscJokes(){
+        given()
+                .when()
+                .get(baseUrl+"/joke/Misc").then()
+                .statusCode(200)
+                .body("category", equalTo("Misc"))
+                .log().all();
+    }
+
+    @Test
+    void testGetAnyJokes(){
+        given()
+                .when()
+                .get(baseUrl+"/joke/Any").then()
+                .statusCode(200)
+                .log().all();
+    }
+
+    @Test
+    void testGetSafeJokes(){
+        given()
+                .when()
+                .get(baseUrl+"/joke/Any?blacklistFlags=religious,political").then()
+                .statusCode(200)
+                .log().all();
+    }
+
+    @Test
+    void testGetSearchJokes(){
+        String keyword = "school";
+        Response response = given()
+                .when()
+                .get(baseUrl+"/joke/Any?contains="+keyword).then()
+                .statusCode(200)
+                .log().all()
+                .extract().response();
+        String setup = response.jsonPath().get("setup");
+        String delivery = response.jsonPath().get("delivery");
+        boolean check1 = (setup.contains(keyword));
+        boolean check2 = (delivery.contains(keyword));
+        Assert.assertTrue((check1||check2));
+    }
+
+    @Test
+    void testGetCheckAmountOfJokes(){
+        int amount = 3;
+        given()
+                .when()
+                .get(baseUrl+"/joke/Any?amount="+amount).then()
+                .statusCode(200)
+                .body("size()", is(amount))
+                .log().all();
+    }
 }
