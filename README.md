2439e11786616535595f85345a2700f2e9272b66
swarnava.18 <swarnava.18@mountblue.tech>
2022-01-18 07:33:11 +0530

    five get methods are added into TestBooksAPI, test-results added into test-result directory

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 - The Users API is available at `https://reqres.in`
 - The Jokes API is available at `https://v2.jokeapi.dev`
+- The Books API is available at `https://simple-books-api.glitch.me`
+
+
+# Test Results #
+
+Test results are available at `test-results\` directory
+
+- Tested Users API result `Test Results - TestUsersAPI.html`
+- Tested Jokes API result `Test Results - TestJokesAPI.html`
+- Tested Books API result `Test Results - TestBooksAPI.html`
--- src/test/java/com/restassured/swarnava/TestBooksAPI.java
+++ src/test/java/com/restassured/swarnava/TestBooksAPI.java
@@ -6,8 +6,7 @@ import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.*;
 
 public class TestBooksAPI {
     String accessToken = "bff6c3e279a31e6f73fdbee51737a77d8768a7ab2d3202ba8aa3610614d5dec6";
@@ -20,7 +19,7 @@ public class TestBooksAPI {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("clientName", "Swarnava Chakraborty");
         jsonObject.put("clientEmail", randomMail);
-        Response response =  given()
+        given()
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
                 .body(jsonObject.toJSONString())
@@ -29,8 +28,7 @@ public class TestBooksAPI {
                 .then()
                 .statusCode(201)
                 .body("$", hasKey("accessToken"))
-                .log().all()
-                .extract().response();
+                .log().all();
     }
 
     @Test(priority = 2)
@@ -55,16 +53,88 @@ public class TestBooksAPI {
         orderId = response.jsonPath().get("orderId");
     }
 
-    /*
-    all get methods will implemented here
-     */
-
     @Test(priority = 3)
+    void testGetStatus() {
+        String url = "https://simple-books-api.glitch.me/status/";
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .get(url)
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("OK"))
+                .log().all();
+    }
+
+    @Test(priority = 4)
+    void testGetBooks() {
+        String url = "https://simple-books-api.glitch.me/books/";
+        String type = "fiction"; //fiction or non-fiction
+        int limit = 3; // 1 to 20
+        given()
+                .contentType(ContentType.JSON)
+                .param("type", type)
+                .param("limit", limit)
+                .when()
+                .get(url)
+                .then()
+                .statusCode(200)
+                .body("size()", equalTo(limit))
+                .log().all();
+    }
+
+    @Test(priority = 5)
+    void testGetSingleBook() {
+        int bookId = 5;
+        String url = "https://simple-books-api.glitch.me/books/{bookId}";
+        given()
+                .contentType(ContentType.JSON)
+                .pathParam("bookId", bookId)
+                .accept(ContentType.JSON)
+                .when()
+                .get(url)
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(bookId))
+                .log().all();
+    }
+
+    @Test(priority = 6)
+    void testGetOrders() {
+        String url = "https://simple-books-api.glitch.me/orders";
+        given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer "+accessToken)
+                .when()
+                .get(url)
+                .then()
+                .statusCode(200)
+                .body("size()", greaterThan(0))
+                .log().all();
+    }
+
+    @Test(priority = 7)
+    void testGetSingleOrder() {
+        String url = "https://simple-books-api.glitch.me/orders/{orderId}";
+        given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer "+accessToken)
+                .pathParam("orderId", orderId)
+                .when()
+                .get(url)
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(orderId))
+                .log().all();
+    }
+
+    @Test(priority = 8)
     void testPatchOrder() {
         String url = "https://simple-books-api.glitch.me/orders/"+orderId;
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("customerName", "Pk2");
-        Response response =  given()
+        given()
                 .header("Authorization", "Bearer "+accessToken)
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
@@ -73,14 +143,13 @@ public class TestBooksAPI {
                 .patch(url)
                 .then()
                 .statusCode(204)
-                .log().all()
-                .extract().response();
+                .log().all();
     }
 
-    @Test(priority = 4)
+    @Test(priority = 9)
     void testDeleteOrder() {
         String url = "https://simple-books-api.glitch.me/orders/"+orderId;
-        Response response =  given()
+        given()
                 .header("Authorization", "Bearer "+accessToken)
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
@@ -88,7 +157,6 @@ public class TestBooksAPI {
                 .delete(url)
                 .then()
                 .statusCode(204)
-                .log().all()
-                .extract().response();
+                .log().all();
     }
 }
